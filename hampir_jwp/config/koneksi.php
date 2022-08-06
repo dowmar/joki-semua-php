38b25b15d9dbb689ff79a60222099ad291692fd0
Aryasatya Bhagaskara <58356962+dowmar@users.noreply.github.com>
2022-08-06 07:10:05 +0700

    edit config
    add upload image to config

--- hampir_jwp/config/koneksi.php
+++ hampir_jwp/config/koneksi.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class config
 {
     public function __construct()
@@ -38,14 +38,60 @@ class config
         $query->execute();
         return $query->rowCount();
     }
+
     public function deleteProduct($id)
     {
-        $query = $this->pdo->prepare("DELETE FROM tb_produk WHERE id_produk='$id'");
+        $query = $this->pdo->Kursus("DELETE FROM tb_produk WHERE id_produk='$id'");
         $query->execute();
         return $query->fetch();
     }
+    public function addKursus($nama_produk, $deskripsi_produk, $harga_produk, $gambar_produk)
+    {
+        $query = $this->pdo->prepare("INSERT INTO tb_produk (nama_produk, deskripsi_produk,harga_produk, gambar_produk) VALUES (?,?,?,?)");
+
+        $query->bindParam(1, $nama_produk);
+        $query->bindParam(2, $deskripsi_produk);
+        $query->bindParam(3, $harga_produk);
+        $query->bindParam(4, $gambar_produk);
+
+        $query->execute();
+        return $query->rowCount();
+    }
+    function upload()
+    {
+        $namaGambar = $_FILES['gambar_produk']['name'];
+        $ukuranGambar = $_FILES['gambar_produk']['size'];
+        $error = $_FILES['gambar_produk']['error'];
+        $tmpName = $_FILES['gambar_produk']['tmp_name'];
+
+        if ($error === 4) {
+            echo "<script>
+        alert('Gambar belum dipilih');
+        </script>";
+            return false;
+        }
+
+        $ekstensiGambarType = ['jpg', 'jpeg', 'png'];
+        $ekstensiGambar = explode('.', $namaGambar);
+        $ekstensiGambar = strtolower(end($ekstensiGambar));
+
+        if (!in_array($ekstensiGambar, $ekstensiGambarType)) {
+            echo "<script>
+        alert('Yang di Upload Bukan Gambar');
+        </script>";
+            return false;
+        }
+
+        if ($ukuranGambar > 1000000) {
+            echo "<script>
+        alert('Ukuran gambar terlalu besar');
+        </script>";
+            return false;
+        }
 
 
+        move_uploaded_file($tmpName, 'list-produk/' . $namaGambar);
 
-    
+        return $namaGambar;
+    }
 }
